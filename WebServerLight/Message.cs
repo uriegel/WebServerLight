e79576a1a315e2301f2999451e164ee4295a1f9b
uriegel <uriegel@hotmail.de>
2025-02-09 18:36:08 +0100

    Read HTTP msg with already read binary buffer

--- WebServerLight/Message.cs
+++ WebServerLight/Message.cs
@@ -1,33 +1,63 @@
+using System.Collections;
+using System.Collections.Immutable;
 using System.Text;
-
-using static System.Console;
+using CsTools.Extensions;
 
 namespace WebServerLight;
 
 class Message
 {
+    public Method Method { get; }
+    public string Url { get; }
     public static async Task<Message?> Read(Server server, Stream networkStream, CancellationToken cancellation)
     {
-        using var reader = new StreamReader(networkStream, Encoding.UTF8, leaveOpen: true, bufferSize: 8192);
-        var request = await reader.ReadLineAsync();
+        var buffer = new byte[8192];
+        int bytesRead = 0;
+        int totalBytes = 0;
+        var headerBuffer = new MemoryStream(4096);
+        while ((bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length, cancellation)) > 0)
+        {
+            headerBuffer.Write(buffer, 0, bytesRead);
+            totalBytes += bytesRead;
 
-        var headers = GetHeaders(reader).ToDictionary();
-        WriteLine(headers);
+            // Convert buffer to string & check for header termination
+            string headerText = Encoding.UTF8.GetString(headerBuffer.ToArray());
 
-        // TODO check POST request, read binary
-
+            int headerEndIndex = headerText.IndexOf("\r\n\r\n", StringComparison.Ordinal);
+            if (headerEndIndex < 0)
+                headerEndIndex = headerText.IndexOf("\n\n", StringComparison.Ordinal);
+            if (headerEndIndex >= 0)
+            {
+                headerEndIndex += headerText[headerEndIndex] == '\r' ? 4 : 2; // Adjust index for "\r\n\r\n" or "\n\n"
+                return new Message(headerText[..headerEndIndex], buffer, headerEndIndex);
+            }
+        }
         return null;
     }
 
-    static IEnumerable<KeyValuePair<string, string>> GetHeaders(StreamReader reader)
+    public Message(string headerPart, byte[] buffer, int payloadBegin)
     {
-        while (true)
+        var parts = headerPart.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+        var method = parts[0].SubstringUntil(' ');
+        Method = method switch
         {
-            var headerLine = reader.ReadLine();
-            if (string.IsNullOrWhiteSpace(headerLine))
-                break;
-            var parts = headerLine.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            yield return new(parts[0], parts[1]);
-        }
+            "GET" => Method.Get,
+            "POST" => Method.Post,
+            "PUT" => Method.Put,
+            "Delete" => Method.Delete,
+            _ => throw new Exception($"HTTP method {method} not supported")
+        };
+        Url = parts[0].StringBetween(" ", " ");
+        var affen = parts.Skip(1).Select(MakeHeader).ToImmutableDictionary(StringComparer.OrdinalIgnoreCase);
+        var affe = affen;
+        var länge = affen.GetValueOrDefault("affe");
+         länge = affen.GetValueOrDefault("Content-Length");
+         länge = affen.GetValueOrDefault("content-length");
+    }
+
+    static KeyValuePair<string, string> MakeHeader(string headerLine)
+    {
+        var parts = headerLine.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        return new(parts[0], parts[1]);
     }
 }
---
+++ WebServerLight/Method.cs
@@ -0,0 +1,9 @@
+namespace WebServerLight;
+
+public enum Method
+{
+    Get,
+    Post,
+    Put,
+    Delete
+}
