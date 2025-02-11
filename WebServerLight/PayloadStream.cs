605c879d86b63c92ba124b6de7125c3879f01837
Uwe Riegel <uriegel@hotmail.de>
2025-02-11 13:08:29 +0100

    StreamBase from CsTools

--- WebServerLight/PayloadStream.cs
+++ WebServerLight/PayloadStream.cs
@@ -1,16 +1,16 @@
+using CsTools;
+
 namespace WebServerLight;
 
-class PayloadStream(Memory<byte> payloadBegin, Stream networkStream, int length) : Streambase
+class PayloadStream(Memory<byte> payloadBegin, Stream networkStream, int length) : StreamBase
 {
+    public override bool CanRead => true;
+
     public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
     {
         var pos = Math.Min(buffer.Length, payloadBegin.Length);
         var copyBuffer = payloadBegin[..pos];
         copyBuffer.CopyTo(buffer);
-
-
-        var test = System.Text.Encoding.UTF8.GetString(buffer[..pos].Span);
-
         payloadBegin = payloadBegin[pos..];
         return ValueTask.FromResult(pos);
     }
