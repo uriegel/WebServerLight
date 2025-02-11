10814a0ad4e3b85afaeed8facdbebbfe0eeb999b
uriegel <uriegel@hotmail.de>
2025-02-11 17:40:16 +0100

    New version

--- TestServer/website/scripts/script.js
+++ TestServer/website/scripts/script.js
@@ -2,7 +2,7 @@ const btn1 = document.getElementById("button")
 const btn2 = document.getElementById("button2")
 
 btn1.onclick = async () => {
-    var res = await request("cmd1", {
+    var res = await jsonPost("cmd1", {
         text: "Text",
         id: 123
     })
@@ -10,7 +10,7 @@ btn1.onclick = async () => {
 }
 
 btn2.onclick = async () => {
-    var res = await request("cmd2", {
+    var res = await jsonPost("cmd2", {
         text: "Text",
         id: 123,
         longArray: Array.from(Array(10_000).keys()).map((n, i) => `Item ${i}`)
@@ -18,7 +18,7 @@ btn2.onclick = async () => {
     console.log("cmd2", res)
 }
 
-async function request(method, input) {
+async function jsonPost(method, input) {
 
     const msg = {
         method: 'POST',
