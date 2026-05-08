fb98087877a874ffa9f9b36b3fb386aeb6d31f16
Uwe Riegel <uriegel@hotmail.de>
2026-05-08 08:01:55 +0200

    Bug in Path.Probe

--- WebServerLight/Routing/PathRoute.cs
+++ WebServerLight/Routing/PathRoute.cs
@@ -9,7 +9,10 @@ public class PathRoute(string path, List<Route> routes) : Route(routes)
     internal PathRoute(string path) : this(path, []) { }
 
     internal override async ValueTask<RouteResult> Probe(IRequest msg, Func<Exception, IRequest, Task>? onException)
-        => msg is Message message && message.Url.StartsWith(path, StringComparison.CurrentCultureIgnoreCase)
+        => msg is Message message && CheckPath(message.Url, path)
                 ? await ProbeRoutes(msg.SideEffect(n => (n as Message)?.SetRequestPath(path)), onException)
                 : RouteResult.Next;
+
+    static bool CheckPath(string urlPath, string routePath)
+        => (urlPath + '/').StartsWith(routePath + '/', StringComparison.CurrentCultureIgnoreCase);
 }
